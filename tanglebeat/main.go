8f509dd3aac8fd1343f6d8789bd841902d72194f
lunfardo314 <evaldas.alt@gmail.com>
2018-10-10 10:01:12 +0300

    metrics skeleton

--- tanglebeat/main.go
+++ tanglebeat/main.go
@@ -8,6 +8,10 @@ func main() {
 	readConfig("tanglebeat.yaml")
 	log.Infof("Will be receiving transaction data from '%v'", Config.SenderURI)
 	initDB()
-
+	//r, err := sumUpBySequence(24 * 60 * 60 * 1000)
+	//if err != nil{
+	//	log.Panic(err)
+	//}
+	//log.Infof("SUMS: %+v", r)
 	runUpdateDb()
 }
--- tanglebeat/metrics.go
+++ tanglebeat/metrics.go
@@ -1,11 +1,51 @@
 package main
 
 type sums struct {
-	count             int
-	confDurationsMsec int64
-	powDurationsMsec  int64
+	count              int64
+	confDurationsMsec  int64
+	powDurationsMsec   int64
+	tipselDurationMsec int64
+	numTransactions    int64
 }
 
-func sumUpBySequence() {
+// TODO filtering rows
+var sqlSelect = `select seqid, 
+		count(*), 
+		sum(last_update_msec-started_ts_msec), 
+		sum(total_pow_duration_msec),
+		sum(total_tipsel_duration_msec),
+		sum(num_attaches * bundle_size + num_promotions * promo_bundle_size)
+	 from transfers
+	 where last_update_msec >= ?
+	`
 
+func sumUpBySequence(lastMsec int64) (map[string]sums, error) {
+	ret := make(map[string]sums)
+	rows, err := dbconn.Query(sqlSelect, lastMsec)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var seqid string
+	var count int64
+	var confDurationsMsec int64
+	var powDurationsMsec int64
+	var tipselDurationsMsec int64
+	var numTransactions int64
+
+	for rows.Next() {
+		err = rows.Scan(&seqid, &count, &confDurationsMsec, &powDurationsMsec, &tipselDurationsMsec, &numTransactions)
+		if err != nil {
+			return nil, err
+		}
+		ret[seqid] = sums{
+			count:              count,
+			confDurationsMsec:  confDurationsMsec,
+			powDurationsMsec:   powDurationsMsec,
+			tipselDurationMsec: tipselDurationsMsec,
+			numTransactions:    numTransactions,
+		}
+	}
+	return ret, nil
 }
