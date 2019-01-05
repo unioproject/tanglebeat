3783b3e8e12c22f428a38f17673af3b096f40334
lunfardo314 <evaldas.alt@gmail.com>
2019-01-05 20:40:42 +0200

    tbsender publisher

--- tbsender/publisher.go
+++ tbsender/publisher.go
@@ -8,7 +8,8 @@ var updatePublisher *nanomsg.Publisher
 
 func mustInitAndRunPublisher() {
 	var err error
-	updatePublisher, err = nanomsg.NewPublisher(Config.SenderUpdatePublisher.OutputPort, 0, log)
+	updatePublisher, err = nanomsg.NewPublisher(
+		Config.SenderUpdatePublisher.Enabled, Config.SenderUpdatePublisher.OutputPort, 0, log)
 	if err != nil {
 		log.Errorf("Failed to create publishing channel. Publisher is disabled: %v", err)
 		Config.SenderUpdatePublisher.Enabled = false
