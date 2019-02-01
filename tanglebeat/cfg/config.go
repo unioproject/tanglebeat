d7a09b53ddd7e51369e0f3d1055d2c40d6efc827
lunfardo314 <evaldas.alt@gmail.com>
2019-02-01 22:11:04 +0200

    ver

--- tanglebeat/cfg/config.go
+++ tanglebeat/cfg/config.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	Version                = "19.01.30-1"
+	Version                = "19.02.01-1"
 	logFormat              = "%{time:2006-01-02 15:04:05.000} %{level:.4s} [%{module:.6s}|%{shortfunc:.12s}] %{message}"
 	level                  = logging.DEBUG
 	onHoldThresholdDefault = 50
