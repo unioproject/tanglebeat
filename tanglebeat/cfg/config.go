f55afd02b42dfdf89816c82d82160ffc6eca5dad
lunfardo314 <evaldas.alt@gmail.com>
2019-05-10 13:27:08 -0300

    multiquorum TPS metrics introduced

--- tanglebeat/cfg/config.go
+++ tanglebeat/cfg/config.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	Version   = "unio 19.04.26-2"
+	Version   = "unio 10.05.26-1"
 	logFormat = "%{time:2006-01-02 15:04:05.000} %{level:.4s} [%{module:.6s}|%{shortfunc:.12s}] %{message}"
 	level     = logging.DEBUG
 )
