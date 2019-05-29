66266dd41d2487ddfdf506f702a8033df397e550
lunfardo314 <evaldas.alt@gmail.com>
2019-05-29 18:40:39 -0300

    added possibility to spawn configurable cmd lines at startup

--- tanglebeat/spawncmd.go
+++ tanglebeat/spawncmd.go
@@ -4,6 +4,7 @@ import (
 	"github.com/unioproject/tanglebeat/tanglebeat/cfg"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 // spawning cmd lines specified in spawnCmd part of the config file
@@ -19,7 +20,12 @@ func spawnCommands() {
 func spawnCmd(cmdline string) {
 	infof("Spawning command '%v' from 'tanglebeat'", cmdline)
 
-	cmd := exec.Command(cmdline)
+	words := strings.Split(cmdline, " ")
+	if len(words) == 0 {
+		errorf("Failed to run '%v' from 'tanglebeat': wrong cmdline '%v'", cmdline)
+		return
+	}
+	cmd := exec.Command(words[0], words[1:]...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	go func() {
