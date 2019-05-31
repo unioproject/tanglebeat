01d5c4d7174ae9f2619db31d67b55c11455158dd
lunfardo314 <evaldas.alt@gmail.com>
2019-05-31 10:36:04 -0300

    confirmer bug sending update to closed confirmer update channel

--- examples/readnano/main.go
+++ examples/readnano/main.go
@@ -11,9 +11,9 @@ import (
 
 // program reads Nanomsg stream for testing
 
-const defaultUri = "tcp://test1.tanglebeat.com:5550"
+const defaultUri = "tcp://tanglebeat.com:5550"
 
-var topics = []string{"lmi", "lmhs", "seen"}
+var topics = []string{"lmi", "lmhs", "seen", "tx"}
 
 func main() {
 	pstr := flag.String("uri", defaultUri, "Nanomsg stream URI")
